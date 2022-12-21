72db90921a802d671ebab6f97082e352fd20fbda
Guillermo Barboza <guillermo.barboza@jam3.com>
2022-12-21 02:51:29 -0300

    initial website with a button

---
+++ components/Header/Header.tsx
@@ -0,0 +1,37 @@
+import styles from './Header.module.css'
+import { useEffect, useRef } from 'react'
+import gsap from 'gsap';
+
+export default function Header() {
+
+    const button: React.RefObject<HTMLButtonElement> = useRef(null);
+    const root = useRef();
+    const onClick = () => {
+
+        gsap.to(root.current, { rotation: "+=360" });
+    }
+
+    useEffect(() => {
+
+
+    }, [])
+
+    return (
+        <>
+            <header className={styles.header}>
+                <div className={styles.content} >
+                    <h1 ref={root} className={styles.h1}>This is a Website</h1>
+                    <button ref={button} onClick={onClick}>Clicky Clicky</button>
+                </div>
+
+                <nav className={styles.nav}><h3>Where do you wanna go?</h3>
+                    <ul>
+                        <li><button>to the m00n...</button></li>
+                        <li><button>I wanna go biking</button></li>
+                        <li><button>just a chill trip</button></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
