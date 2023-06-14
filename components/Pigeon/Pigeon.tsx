6ebc746f225482d77acd2e904bc8794128d0616d
Guillermo Barboza <guillermo.barboza@jam3.com>
2023-06-14 18:51:17 -0300

    test: change it to a cube

--- components/Pigeon/Pigeon.tsx
+++ components/Pigeon/Pigeon.tsx
@@ -1,15 +1,34 @@
 import { useRef } from "react";
 import { Mesh } from "three";
 import { useLoader } from "@react-three/fiber";
+import { useEffect } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-
-const gltf = useLoader(GLTFLoader, "./public/assets/pigeon/scene.gltf");
+import GUI from "lil-gui";
 
 export default function Pigeon() {
   const meshRef = useRef<Mesh>(null);
+  const positionValues = { x: 1, y: 1, z: -1 };
+  const scaleValues = { scale: 1 };
+
+  const gltf = useLoader(GLTFLoader, "/assets/pigeon/scene.gltf");
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const gui = new GUI();
+
+      gui.add(positionValues, "x", -200, 200);
+      gui.add(positionValues, "y", -200, 200);
+      gui.add(positionValues, "z", -200, 200);
+      gui.add(scaleValues, "scale", 0, 10);
+    }
+  }, []);
 
   return (
-    <mesh ref={meshRef} position={[0.7, 1, -1]}>
+    <mesh
+      ref={meshRef}
+      position={[positionValues.x, positionValues.y, positionValues.z]}
+      scale={scaleValues.scale}
+    >
       <primitive object={gltf.scene} />
     </mesh>
   );
