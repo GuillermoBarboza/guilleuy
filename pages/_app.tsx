45585e3b8d29930ed9a2544b43a7f029074664ff
Guillermo Barboza <gui.bar.jim@gmail.com>
2023-12-18 01:55:19 -0300

    sst init

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { CommonLayout } from '../layouts/CommonLayout/CommonLayout'
-
-export default function App(props: AppProps) {
-  return <CommonLayout {...props} />
-}
-
